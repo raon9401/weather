6b21696ee961f90059edd067b098cd38e64ea5e8
raon9401 <raon9401@naver.com>
2024-03-25 01:18:09 +0900

    Feat: 위치정보 권한 멘트 추가

--- src/component/WeatherBox.jsx
+++ src/component/WeatherBox.jsx
@@ -3,13 +3,14 @@ import Loading from './Loading';
 
 const WeatherBox = ({ weatherInfo, isLoading }) => {
   // 화씨 = 섭씨 * 1.8 + 32
+  console.log(weatherInfo)
   const calcFahrenheit = (fahrenheit) => {
     return (fahrenheit * 1.8 + 32).toFixed(3);
   }
 
   return (
     <div className='weather-info-box'>
-      {isLoading ?
+      {isLoading && weatherInfo ?
         <>
           <span>
             {weatherInfo?.name}
@@ -21,11 +22,13 @@ const WeatherBox = ({ weatherInfo, isLoading }) => {
           </div>
           <h3>
             {weatherInfo?.weather[0].description}
+            <img src={`https://openweathermap.org/img/wn/${weatherInfo?.weather[0].icon}.png`} alt="weather icon" />
           </h3>
         </>
         :
         <>
           <Loading />
+          {!weatherInfo && <h5>위치 정보 권한을 허용해주세요!</h5>}
         </>
       }
     </div>
